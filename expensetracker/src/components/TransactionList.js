9fc7543b0b6aff5d5142e7b1268313d0150e58aa
daogen <daoyeyemi@aol.com>
2021-03-05 03:23:08 -0800

    made changes to mapping of transaction component

--- expensetracker/src/components/TransactionList.js
+++ expensetracker/src/components/TransactionList.js
@@ -13,7 +13,7 @@ const TransactionList = () => {
             <h3>History</h3>
             <ul id="list" className="list">
                 {transactions.map(transaction => (
-                <Transaction />))}
+                <Transaction key={transaction.id} transaction={transaction}/>))}
                 
             </ul>
         </div>
