38737ac80aeb11055afe5b306365913c02facc87
daogen <daoyeyemi@aol.com>
2021-03-05 03:19:25 -0800

    added Transaction component and added it to transaction list component

---
+++ expensetracker/src/components/Transaction.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Transaction() {
+    return (
+        <li className="minus">
+            {transaction.text} <span>{transaction.amount}</span>
+            <button className="delete-btn">x</button>
+        </li>
+    )
+}
+
+export default Transaction
